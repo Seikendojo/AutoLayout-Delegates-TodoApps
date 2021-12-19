656526195ef21bf2c1dc9c607ced311f624a72ba
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2021-12-18 18:47:36 -0800

    Create Views and UI elements
    Create home and input view controllers and UI elelments

---
+++ ToDos/ToDos/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  ToDos
+//
+//  Created by Sia on 2021-12-18.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    @IBOutlet var whatToDoTextFeild: UITextField!
+    @IBOutlet var dateTextFeild: UITextField!
+    @IBOutlet var priorityControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func configureSegment(_ sender: UISegmentedControl) {
+    
+        let selectedIndex = priorityControl.isSelected
+        if !selectedIndex {
+            priorityControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
+        }
+    }
+}
--- ToDos/ToDos/TodosViewController.swift
+++ ToDos/ToDos/TodosViewController.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class TodosViewController: UITableViewController {
+    
+    @IBOutlet var nothingTodoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
 
+
+// MARK: - DataSource
+
+extension TodosViewController {
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Task"
+        nothingTodoLabel.isHidden = true
+        return cell
+    }
+
+    
 }
