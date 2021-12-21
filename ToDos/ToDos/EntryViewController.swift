942ca031887b80c8437b903a3dc9f36fbb3f7da9
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2021-12-20 23:42:55 -0800

    Configure Date Picker

--- ToDos/ToDos/EntryViewController.swift
+++ ToDos/ToDos/EntryViewController.swift
@@ -13,15 +13,41 @@ class EntryViewController: UIViewController {
     @IBOutlet var dateTextFeild: UITextField!
     @IBOutlet var priorityControl: UISegmentedControl!
     
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         whatToDoTextFeild.becomeFirstResponder()
+        configDatePicker()
     }
     
+    func configToolbar() -> UIToolbar {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolBar.setItems([doneButton], animated: true)
+
+        return toolBar
+    }
+    
+    func configDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        dateTextFeild.inputView = datePicker
+        dateTextFeild.inputAccessoryView = configToolbar()
+        dateTextFeild.textAlignment = .center
+    }
+    
+    @objc func donePressed() {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        dateTextFeild.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
     
     @IBAction func configureSegment(_ sender: UISegmentedControl) {
-    
         let selectedIndex = priorityControl.isSelected
         if !selectedIndex {
             priorityControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
